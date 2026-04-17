3c8025aaedb2e23c3f36f6304a5202d71ba01a13
tejashree <tejashreek@gmail.com>
2026-04-17 12:53:11 -0700

    review imports

--- app/src/main/java/nl/jovmit/countries/ui/screen/SearchScreen.kt
+++ app/src/main/java/nl/jovmit/countries/ui/screen/SearchScreen.kt
@@ -20,11 +20,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nl.jovmit.countries.R
-import androidx.compose.ui.res.stringResource
 
 data class ProductUiModel(
     val id: String,
